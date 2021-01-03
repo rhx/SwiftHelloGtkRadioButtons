90be1d36959bf9610c50f8fd331a79132b7069bd
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-04 09:48:39 +1000

    Add BUILD_DIR support

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@
 # that the swift wrapper code exists
 #
 . ./config.sh
-gtk=`echo .build/checkouts/SwiftGtk*/Sources/Gtk/Gtk-3.0.swift`
+gtk=`echo $BUILD_DIR/checkouts/SwiftGtk*/Sources/Gtk/Gtk-?.0.swift`
 [ -e $gtk ] || ./generate-wrapper.sh
-exec swift test "$@"
+exec swift test -build-path "$BUILD_DIR" "$@"
