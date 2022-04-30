ddb4e8eec8f1384bf37b0164e033c685d1a27203
Rene Hexel <r.hexel@griffith.edu.au>
2022-05-01 06:48:29 +1000

    Upgrade to Swift 5.6 and use plugin

--- test.sh
+++ test.sh
@@ -4,6 +4,4 @@
 # that the swift wrapper code exists
 #
 . ./config.sh
-gtk=`echo $BUILD_DIR/checkouts/SwiftGtk*/Sources/Gtk/Gtk-?.0.swift`
-[ -e $gtk ] || ./generate-wrapper.sh
 exec swift test -build-path "$BUILD_DIR" "$@"
